13452b565dc6b10aa3aa7983b39b24f92f8863a9
AVIPAGHADAR1729 <72063788+AVIPAGHADAR1729@users.noreply.github.com>
2021-10-05 16:13:52 +0530

    Add files via upload

---
+++ BalanceSymbol.py
@@ -0,0 +1,39 @@
+
+
+
+symbol = {
+  '[' : -1,'{' : -2,'(' : -3,'<':-4,']' : 1,'}' : 2,')' : 3,'>' : 4
+}
+
+
+print(symbol)
+
+
+def isBalanced(inp):
+    stack = []
+
+    for e in inp:
+
+        if symbol[e] < 0:
+            stack.append(e)
+        else:
+
+            if stack == []:
+                return "NO"
+            else:
+                top = stack.pop()
+                if symbol[top] + symbol[e] != 0:
+                    return "NO"
+
+    if stack == []:
+        return "YES"
+    else:
+        return "NO"
+
+
+
+
+
+print(isBalanced("{{}(){}}<>{}{{<<>>}}{}"))
+
+            
