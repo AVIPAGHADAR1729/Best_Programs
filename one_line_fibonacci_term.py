1365e82ed6040ed4e4b624930a0517d8466147c3
AVIPAGHADAR1729 <72063788+AVIPAGHADAR1729@users.noreply.github.com>
2021-10-06 12:53:58 +0530

    Create one_line_fibonacci_term.py

---
+++ one_line_fibonacci_term.py
@@ -0,0 +1,11 @@
+# Fibonacci term With One Line Lambda Function 
+
+
+fibo = lambda n: round((((1+5**0.5)/2)**n)/5**0.5)
+    
+print(fibo(10))
+print(fibo(500))
+
+# Time Complexity :- O(log(n))
+# Space Complexity : O(1)
+# Limit of term is max 1474 term is Calculated wuth this Function 
